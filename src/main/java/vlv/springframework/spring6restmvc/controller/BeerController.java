fbd963584bf6794ebb81fdd87901225d1f64269c
VLV01 <vinicius.linsvasco@hotmail.com>
2023-03-28 14:20:13 -0300

    HTTP GET with Spring MVC List Operation

--- src/main/java/vlv/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/vlv/springframework/spring6restmvc/controller/BeerController.java
@@ -2,17 +2,26 @@ package vlv.springframework.spring6restmvc.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import vlv.springframework.spring6restmvc.model.Beer;
 import vlv.springframework.spring6restmvc.services.BeerService;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController
 public class BeerController {
     private final BeerService beerService;
+
+    @RequestMapping("api/v1/beer")
+    public List<Beer> listBeers() {
+        return beerService.listBeers();
+    }
 
     public Beer getBeerById(UUID id) {
 
