14d4b8276f768c708bf2a0de05ef648ba42bb809
VLV01 <vinicius.linsvasco@hotmail.com>
2023-03-28 17:08:19 -0300

    Assignment - GET Customer

---
+++ src/main/java/vlv/springframework/spring6restmvc/services/CustomerService.java
@@ -0,0 +1,13 @@
+package vlv.springframework.spring6restmvc.services;
+
+import vlv.springframework.spring6restmvc.model.Customer;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CustomerService {
+    Customer getCustomerById(UUID uuid);
+
+    List<Customer> getAllCustomers();
+
+}
