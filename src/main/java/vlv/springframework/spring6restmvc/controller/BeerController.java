7dc573b79eae7231ab9b87d8cf36c03be0537ce9
VLV01 <vinicius.linsvasco@hotmail.com>
2023-03-29 17:16:43 -0300

    Set Header on HTTP Response

--- src/main/java/vlv/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/vlv/springframework/spring6restmvc/controller/BeerController.java
@@ -2,11 +2,9 @@ package vlv.springframework.spring6restmvc.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import vlv.springframework.spring6restmvc.model.Beer;
 import vlv.springframework.spring6restmvc.services.BeerService;
@@ -27,7 +25,10 @@ public class BeerController {
 
         Beer savedBeer = beerService.saveNewBeer(beer);
 
-        return new ResponseEntity(HttpStatus.CREATED);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/beer/" + savedBeer.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 
     @RequestMapping(method = RequestMethod.GET)
