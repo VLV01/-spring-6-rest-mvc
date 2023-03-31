71dfeb79ab225f6b0d57f60de995461b9e99fb7c
VLV01 <vinicius.linsvasco@hotmail.com>
2023-03-31 15:27:13 -0300

    HTTP PATCH with Spring MVC

--- src/main/java/vlv/springframework/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/vlv/springframework/spring6restmvc/services/BeerServiceImpl.java
@@ -2,6 +2,7 @@ package vlv.springframework.spring6restmvc.services;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import vlv.springframework.spring6restmvc.model.Beer;
 import vlv.springframework.spring6restmvc.model.BeerStyle;
 
@@ -13,6 +14,33 @@ import java.util.*;
 @Service
 public class BeerServiceImpl implements BeerService {
     private Map<UUID, Beer> beerMap;
+
+    @Override
+    public void patchBeerById(UUID beerId, Beer beer) {
+        Beer existing = beerMap.get(beerId);
+
+        if (StringUtils.hasText(beer.getBeerName())) {
+            existing.setBeerName(beer.getBeerName());
+        }
+
+        if (beer.getBeerStyle() != null) {
+            existing.setBeerStyle(beer.getBeerStyle());
+        }
+
+        if (beer.getPrice() != null) {
+            existing.setPrice(beer.getPrice());
+        }
+
+        if (beer.getQuantityOnHand() != null) {
+            existing.setQuantityOnHand(beer.getQuantityOnHand());
+        }
+
+        if (StringUtils.hasText(beer.getUpc())) {
+            existing.setUpc(beer.getUpc());
+        }
+
+
+    }
 
     public BeerServiceImpl() {
         this.beerMap = new HashMap<>();
