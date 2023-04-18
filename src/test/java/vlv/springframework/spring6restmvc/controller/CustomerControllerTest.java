1ddaad83a9c131f284ecbd7aab6356e613c49809
VLV01 <vinicius.linsvasco@hotmail.com>
2023-04-18 16:41:22 -0300

    Assignment - Test Update Customer

--- src/test/java/vlv/springframework/spring6restmvc/controller/CustomerControllerTest.java
+++ src/test/java/vlv/springframework/spring6restmvc/controller/CustomerControllerTest.java
@@ -2,6 +2,7 @@ package vlv.springframework.spring6restmvc.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import vlv.springframework.spring6restmvc.model.Beer;
 import vlv.springframework.spring6restmvc.model.Customer;
 import vlv.springframework.spring6restmvc.services.CustomerService;
 import vlv.springframework.spring6restmvc.services.CustomerServiceImpl;
@@ -13,11 +14,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.UUID;
+
 import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import static org.mockito.BDDMockito.given;
@@ -40,6 +43,19 @@ class CustomerControllerTest {
     @BeforeEach
     void setUp() {
         customerServiceImpl = new CustomerServiceImpl();
+    }
+
+    @Test
+    void testUpdateCustomer() throws Exception {
+        Customer customer = customerServiceImpl.getAllCustomers().get(0);
+
+        mockMvc.perform(put("/api/v1/customer/" + customer.getId())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(customer)))
+                .andExpect(status().isNoContent());
+
+        verify(customerService).updateCustomerById(any(UUID.class), any(Customer.class));
     }
 
     @Test
