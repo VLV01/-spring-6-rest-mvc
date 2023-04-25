a67b1c9173325843a2465c6cd54c28a0d603c7ec
VLV01 <vinicius.linsvasco@hotmail.com>
2023-04-25 16:32:36 -0300

    Creating JPA Entities

---
+++ src/main/java/vlv/springframework/spring6restmvc/entities/Beer.java
@@ -0,0 +1,27 @@
+package vlv.springframework.spring6restmvc.entities;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+import vlv.springframework.spring6restmvc.model.BeerStyle;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Beer {
+    private UUID id;
+    private Integer version;
+    private String beerName;
+    private BeerStyle beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime updateDate;
+}
