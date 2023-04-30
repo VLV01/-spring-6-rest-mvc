6f1f6808e931690669737bc60822ac06f368877c
VLV01 <vinicius.linsvasco@hotmail.com>
2023-04-30 19:16:01 -0300

    Assignment - Implement Customer Get Operations

--- src/main/java/vlv/springframework/spring6restmvc/services/CustomerServiceJPA.java
+++ src/main/java/vlv/springframework/spring6restmvc/services/CustomerServiceJPA.java
@@ -10,6 +10,7 @@ import vlv.springframework.spring6restmvc.repositories.CustomerRepository;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @Primary
@@ -19,12 +20,16 @@ public class CustomerServiceJPA implements CustomerService {
     private final CustomerMapper customerMapper;
     @Override
     public Optional<CustomerDTO> getCustomerById(UUID uuid) {
-        return Optional.empty();
+        return Optional.ofNullable(customerMapper
+                .customerToCustomerDto(customerRepository.findById(uuid).orElse(null)));
     }
 
     @Override
     public List<CustomerDTO> getAllCustomers() {
-        return null;
+        return customerRepository
+                .findAll().stream()
+                .map(customerMapper::customerToCustomerDto)
+                .collect(Collectors.toList());
     }
 
     @Override
---
+++ src/test/java/vlv/springframework/spring6restmvc/controller/CustomerControllerIT.java
@@ -0,0 +1,63 @@
+package vlv.springframework.spring6restmvc.controller;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import vlv.springframework.spring6restmvc.entities.Beer;
+import vlv.springframework.spring6restmvc.entities.Customer;
+import vlv.springframework.spring6restmvc.model.BeerDTO;
+import vlv.springframework.spring6restmvc.model.CustomerDTO;
+import vlv.springframework.spring6restmvc.repositories.CustomerRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CustomerControllerIT {
+
+    @Autowired
+    CustomerController customerController;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    void testGetByIdNotFound() {
+        assertThrows(NotFoundException.class, () -> {
+            customerController.getCustomerById(UUID.randomUUID());
+        });
+    }
+
+    @Test
+    void testGetById() {
+        Customer customer = customerRepository.findAll().get(0);
+
+        CustomerDTO dtos = customerController.getCustomerById(customer.getId());
+
+        assertThat(dtos).isNotNull();
+    }
+
+    @Test
+    void testListAll() {
+        List<CustomerDTO> dtos = customerController.listAllCustomers();
+
+        assertThat(dtos.size()).isEqualTo(3);
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testListAllEmptyList() {
+        customerRepository.deleteAll();
+        List<CustomerDTO> dtos = customerController.listAllCustomers();
+
+        assertThat(dtos.size()).isEqualTo(0);
+
+        }
+
+}
