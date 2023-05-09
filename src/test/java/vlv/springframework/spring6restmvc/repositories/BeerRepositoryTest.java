bdbf426c721f851578ce090f56bdccb543ad7e7e
VLV01 <vinicius.linsvasco@hotmail.com>
2023-05-09 17:13:03 -0300

    JPA Validation

--- src/test/java/vlv/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/vlv/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import vlv.springframework.spring6restmvc.entities.Beer;
+import vlv.springframework.spring6restmvc.model.BeerStyle;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,7 +21,12 @@ class BeerRepositoryTest {
     void testSaveBeer() {
         Beer savedBeer = beerRepository.save(Beer.builder()
                         .beerName("My Beer")
+                        .beerStyle(BeerStyle.PALE_ALE)
+                        .upc("88851115665")
+                        .price(new BigDecimal("11.99"))
                 .build());
+
+        beerRepository.flush();
 
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
